2cb759003927490de2c0b134bcea64c12fb34687
pfisterb <ben.jj.pfister@gmail.com>
2021-06-23 19:21:41 +0200

    Store unacknowledged messages

--- zmq-client.py
+++ zmq-client.py
@@ -5,12 +5,15 @@ import random
 from argparse import ArgumentParser
 
 REQUEST_TIMEOUT = 1000  # msecs
-SETTLE_DELAY = 2000  # before failing over
+
+# dictionary to store unacknowledged messages
+messages = {}
 
 def get_message():
     msg = ""
     #+ random.randrange(-3,3)
-    if (int(time.time())+ random.randrange(-3,3))  % 10 == 0: 
+    #if (int(time.time())+ random.randrange(-3,3)) % 10 == 0:
+    if (True):
         msg = "Speed : " + str(random.randrange(70,120))
     return msg
 
@@ -24,7 +27,6 @@ def main():
     parser.add_argument("-id", "--id", type=int, default=1)
     args = parser.parse_args()
 
-
     server = ['tcp://localhost:5001', 'tcp://localhost:5002']
     server_nbr = 0
     ctx = zmq.Context()
@@ -32,46 +34,47 @@ def main():
     client.connect(server[server_nbr])
     poller = zmq.Poller()
     poller.register(client, zmq.POLLIN)
-    expect_reply = False
 
     sequence = 0
     while True:
 
-        # Check if we send a message
+        # print number of unsent messages
+        print( "I: Number of unsent messages:", len(messages) )
+
+        # produce a new message
         msg = get_message()
-        if msg != "":
-            client.send_string("%s," % str(args.id) + "%s," % sequence + "%s" % msg)
-            expect_reply = True
 
+        # save message to dictionary and send
+        messages[sequence] = msg
 
-        # Wait for response
-        while expect_reply:
-            socks = dict(poller.poll(REQUEST_TIMEOUT))
-            if socks.get(client) == zmq.POLLIN:
+        # TODO: send all unacknowledged messages
+        client.send_string("%s," % str(args.id) + "%s," % sequence + "%s" % msg)
+        sequence += 1
 
-                # Recieve Response
-                reply = handle_response(client.recv())
-                
-                if int(reply[1]) == sequence:
-                    print("I: server replied OK (%s)" % reply)
-                    expect_reply = False
-                    sequence += 1
-                    sleep(1)
-                else:
-                    print("E: malformed reply from server: %s" % reply)
-            else:
-                print("W: no response from server, failing over")
-                sleep(SETTLE_DELAY / 1000)
-                poller.unregister(client)
-                client.close()
-                server_nbr = (server_nbr + 1) % 2
-                print("I: connecting to server at %s.." % server[server_nbr])
-                client = ctx.socket(zmq.REQ)
-                poller.register(client, zmq.POLLIN)
-                # reconnect and resend request
-                client.connect(server[server_nbr])
-                client.send_string("%s," % str(args.id) + "%s," % sequence + "%s" % msg)
-        
+        # check if messages are available
+        socks = dict(poller.poll(REQUEST_TIMEOUT))
+        if socks.get(client) == zmq.POLLIN:
+
+            # recieve response
+            reply = handle_response(client.recv())
+            print("I: server replied OK (%s)" % reply)
+
+            # message was delivered, delete from queue
+            del messages[int(reply[1])]
+            sleep(1)
+
+        else:
+            print("W: no response from server, failing over")
+            sleep(1)
+            poller.unregister(client)
+            client.close()
+            server_nbr = (server_nbr + 1) % len(server)
+            print("I: connecting to server at %s.." % server[server_nbr])
+            client = ctx.socket(zmq.REQ)
+            poller.register(client, zmq.POLLIN)
+            # reconnect
+            client.connect(server[server_nbr])
+
         # Sleep shortly when not sending to save CPU from Xploding
         sleep(0.25)
 
